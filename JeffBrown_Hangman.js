803f04d2d4c6ffd60075bb35489a5d450783ac42
Jeffrey Brown <Jeffrey Brown>
2017-02-28 02:15:47 -0500

    cmt

--- JeffBrown_Hangman.js
+++ JeffBrown_Hangman.js
@@ -1,26 +1,58 @@
-      var gameState = "Not Started"
-      var puzzleDisplaytext = document.getElementById("puzzle-state-text");
+var arrayIndex = -1;
+var famoKyians = [];
+var gameState = "Not Started"
+var puzzleDisplaytext = document.getElementById("puzzle-state-text");
+var puzzleSolution = ["j", "e", "n", "n", "i", "f", "e", "r"];
+var puzzleState = [];
+var userGuess = "";
+var userGuessLower = "";
+document.onkeyup = function(event) {
+  if (gameState === "Not Started") {
+    gameState = "MidGame";
+    puzzleState = initState(puzzleSolution);
+    puzzleDisplaytext.textContent = "Guess a letter now, we'll see if its in the puzzle.";
+  } else {
+    userGuess = event.key;
+    userGuessLower = userGuess.toLowerCase();
+    puzzleState = puzzleHandler(puzzleSolution, puzzleState, userGuessLower);
+    puzzleDisplaytext.textContent = writePuzDisTxt(puzzleSolution, puzzleState);
+    arrayIndex = famoKyians.indexOf(userGuessLower);
+    if (arrayIndex === -1) {
+      console.log("index is negative one");
+    } else {
+      console.log("index is " + arrayIndex);
+    }
+  }
+}
 
+function initState(solution) {
+  tbufState = [];
+  for (i = 0; i < solution.length; i++) {tbufState.push(false)}
+  return tbufState;
+}
 
-      var famoKyians = ["j", "e", "n", "n", "i", "f", "e", "r"];
-      var userGuess = "";
-      var arrayIndex = -1;
-      var userGuessLower = "";
-      document.onkeyup = function(event) {
-      	if (gameState === "Not Started") {
-      		console.log("82-85 Fired");
-          gameState = "MidGame";
-		      puzzleDisplaytext.textContent = "Guess a letter now, we'll see if its in the puzzle.";
-        } else {
-        	console.log("87 Fired");
-        	userGuess = event.key;
-        	userGuessLower = userGuess.toLowerCase();
-            puzzleDisplaytext.textContent = event.key;
-        	arrayIndex = famoKyians.indexOf(userGuessLower);
-            if (arrayIndex === -1) {
-	            alert("index is negative one");
-	        } else {
-	            alert("index is " + arrayIndex);
-	        }
-	     }
+function writePuzDisTxt(solution, state) {
+    var tempBuffer = "";
+    console.log("36 state.length = " + state.length);
+    for (i = 0; i < solution.length; i++) {
+      if (state[i]) {
+        tempBuffer = tempBuffer + solution[i] +" ";
+      } else {
+        tempBuffer = tempBuffer + "_ ";
       }
+    }
+    return tempBuffer;
+}
+
+function puzzleHandler(solution, state, guess) {
+  // returns state of puzzle after choice
+  // array of booleans equal length of solution
+  // true if a letter has been matched, false if not
+  // if passed state is empty, initialize all false length of solution
+  for (i = 0; i < solution.length; i++) {
+    if (solution[i] === guess) {
+      state[i] = true;
+    } 
+  }
+  return state;
+}
